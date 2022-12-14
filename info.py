6122240a649d6c533abd588ddd40a388f8174985
amandeep5776 <120557292+amandeep5776@users.noreply.github.com>
2022-12-14 14:36:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -83,6 +83,6 @@ DOWNLOAD_TEXT_URL = "https://t.me/download_from_search_zone_bot"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYT"
+CAPTION_BUTTON_URL = "https://youtube.com"
 
    # Auto Delete For Bot Sending Files #
